a2959b3a396b7c5315c1b5101018c1afcb6830a4
Spencer Henze <spencerhenze@gmail.com>
2017-08-28 13:35:16 -0600

    cleaned up a bit

--- public/src/store/mytunes-store.js
+++ public/src/store/mytunes-store.js
@@ -23,7 +23,6 @@ var store = new vuex.Store({
     },
     updateMyTunes(state, favorites) {
       state.myTunes = favorites
-      console.log(state.myTunes)
     },
     toggleShowMyTunes(state, payload) {
       if (state.showMyTunes == true) {
@@ -40,13 +39,11 @@ var store = new vuex.Store({
       commit('toggleShowMyTunes')
     },
     getMusicByArtist({ commit, dispatch }, artist) {
-      console.log(artist)
       var url = '//bcw-getter.herokuapp.com/?url=';
       var url2 = 'https://itunes.apple.com/search?term=' + artist;
       var apiUrl = url + encodeURIComponent(url2);
 
       $.getJSON(apiUrl).then(data => {
-        console.log(data)
         // at this point the data object from the API includes movies. We need to filter those before committing.
         var filteredList = []
         // check each item to make sure it's a song. if it is, push it to the filtered list.
@@ -64,7 +61,6 @@ var store = new vuex.Store({
             filteredList.push(item)
           }
         })
-        console.log(filteredList)
 
         // now customize the object to make it more friendly.
         var songList = filteredList.map(function (song) {
@@ -89,13 +85,11 @@ var store = new vuex.Store({
         })
 
         // now commit the filtered and customized results
-        console.log(songList)
         commit('setResults', songList)
       })
     },
 
     getMyTunes({ commit, dispatch }) {
-      console.log('hello from getMyTunes')
       //this should send a get request to your server to return the list of saved tunes
       $.get(ip + '/api/mytunes').then(favorites => {
         // favorites should come back sorted
@@ -111,7 +105,6 @@ var store = new vuex.Store({
 
         // Then submit the new song.
         $.post(ip + '/api/mytunes', track).then(addedTrack => {
-          console.log(track)
           dispatch('getMyTunes')
         }).fail(err => { console.error(err) })
 
